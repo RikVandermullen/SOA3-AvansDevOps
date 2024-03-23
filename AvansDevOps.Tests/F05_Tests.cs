62509e8da13d11ab63352a8481cb25457db265e6
RikVandermullen <rik.vandermullen@gmail.com>
2024-03-23 20:46:03 +0100

    Added Invalid BacklogItem State Tests

--- AvansDevOps.Tests/F05_Tests.cs
+++ AvansDevOps.Tests/F05_Tests.cs
@@ -210,5 +210,357 @@ namespace AvansDevOps.Tests
             // Assert
             Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
         }
+
+        #region ToDoState Tests
+
+        [Fact]
+        public void Should_ThrowWhenToDoToReadyForTesting()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToReadyForTesting());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenToDoToTesting()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenToDoToTested()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTested());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenToDoToDone()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenToDoToClosed()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        #endregion
+
+        #region DoingState Tests
+
+        [Fact]
+        public void Should_ThrowWhenDoingToToDo()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToToDo());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenDoingToTesting()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenDoingToTested()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTested());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenDoingToDone()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenDoingToClosed()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        #endregion
+
+        #region ReadyForTesting Tests
+
+        [Fact]
+        public void Should_ThrowWhenReadyForTestingToTodo()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToToDo());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenReadyForTestingDoing()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDoing());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenReadyForTestingDone()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenReadyForTestingClosed()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        #endregion
+
+        #region Invalid State Transitiion
+
+        [Fact]
+        public void Should_ThrowWhenTestingToInvalidState()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+            backlogItem.SetToTesting();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToToDo());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDoing());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenTestedToInvalidState()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+            backlogItem.SetToTesting();
+            backlogItem.SetToTested();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToToDo());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDoing());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTested());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenDoneToInvalidState()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+            backlogItem.SetToTesting();
+            backlogItem.SetToTested();
+            backlogItem.SetToDone();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDoing());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToReadyForTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTested());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+        }
+
+        [Fact]
+        public void Should_ThrowWhenClosedToInvalidState()
+        {
+            // Assert
+            UserFactory userFactory = new DeveloperUserFactory();
+            User dev1 = userFactory.CreateUser("dev1", "dev1@mail", "dev1slack");
+
+            SprintFactory sprintFactory = new ReleaseSprintFactory();
+            Sprint sprint = sprintFactory.CreateSprint("test sprint", new DateTime(2024, 3, 23), new DateTime(2024, 3, 31));
+
+            BacklogItem backlogItem = new BacklogItem((Developer)dev1, sprint, "UserAuthorizationFunctionality");
+            backlogItem.SetToDoing();
+            backlogItem.SetToReadyForTesting();
+            backlogItem.SetToTesting();
+            backlogItem.SetToTested();
+            backlogItem.SetToDone();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToToDo());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDoing());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToReadyForTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTesting());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToTested());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToDone());
+            Assert.Throws<InvalidOperationException>(() => backlogItem.SetToClosed());
+        }
+
+        #endregion
     }
 }
